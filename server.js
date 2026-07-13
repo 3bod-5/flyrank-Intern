bc57ec90f4cdf0857e0828e420eba7d43cad2bab
3bod-5 <abodelking0@gmail.com>
2026-07-13 12:50:00 +0400

    send data to website as a json

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('<h1>Welcome to my website</h1>')
-})
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to my website"
+    });
+});
 
-app.get('/home',(req,res)=>{
-    res.send('<h1>Welcome to my Home page</h1>')
-})
+app.get('/home', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to my home page"
+    });
+});
 
 app.listen(3000,
     console.log('server is runnig on port 3000')
